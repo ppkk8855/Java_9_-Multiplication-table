efe07319b7ecf70c396009732e9d0702a8d3992f
ppkk8855 <ppkk8855@gmail.com>
2019-11-06 15:51:39 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,13 @@
+package com.example.lib;
+
+public class MyClass {
+    public static void main ( String[] args ) {
+        for(int i=2;i<10;i++){
+            for(int j=2;j<10;j++){
+                System.out.printf("%d * %d = %d\n",i,j,i*j );
+            }
+            System.out.println("");
+        }
+
+    }
+}
